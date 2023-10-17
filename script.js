42a81797d769a9caeab20349f05fd00c3af4116d
Ruben Nussbaumer <e235522@edu.sbl.ch>
2023-10-17 11:24:11 +0200

    Aufgabe 1-5

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ function changeBackground() {
   const body = document.querySelector("body")
   body.style.backgroundColor = "darkgrey"
 }
+
+function deleteText () {
+  const elemToDeleteText = document.querySelector("#to-delete")
+  elemToDeleteText.textContent = "Inhalt gelöscht"
+}
+
+
+
+function incrementCounter() {
+  const numberElement = document.querySelector("#counter")
+  let num = parseInt(numberElement.textContent)
+  num = num + 1
+  numberElement.textContent = num
+}
+
+function decrementCounter () {}
